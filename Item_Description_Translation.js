f87bd858059c4ebd3dc28431cdc57a5edd0e03c1
expurement <57848001+expurement@users.noreply.github.com>
2019-11-26 01:17:44 -0600

    Update Item_Description_Translation.js

--- Item_Description_Translation.js
+++ Item_Description_Translation.js
@@ -1,4 +1,3 @@
-var translation = {
     "If held by a Farfetch'd or Sirfetch'd, its critical hit ratio is raised by 2 stages.":"大葱鸭或者葱游兵携带后击中要害的等级提高二级",
     "If held by an Altaria, this item allows it to Mega Evolve in battle.":"七夕青鸟携带可进行超级进化",
     "If held by an Abomasnow, this item allows it to Mega Evolve in battle.":"暴雪王携带可进行超级进化",
@@ -360,4 +359,4 @@ var translation = {
     "If held by a Zacian, this item changes it to Crowned Forme.":"苍响携带会变为剑之王形态",
     "If held by a Zamazenta, this item changes it to Crowned Forme.":"藏玛然特携带会变为盾之王形态",
     "Raises holder's Special Attack by 1 stage after using a sound move. Single use.":"使用声音类招式时，特攻提升一级，使用后消失",
-}
+
